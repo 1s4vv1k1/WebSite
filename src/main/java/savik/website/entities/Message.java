91f98623615ddd7bc7513df93d0a825300401694
Savik213 <ssavelij97@gmail.com>
2023-06-01 13:12:51 +0400

    Changed db to postgres, added flyway

--- src/main/java/savik/website/entities/Message.java
+++ src/main/java/savik/website/entities/Message.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 @Entity
 public class Message {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     private String text;
